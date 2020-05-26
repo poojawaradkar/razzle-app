343da78eb84ccb88bc9612414a71d69217aafd47
poojawaradkar <pooja.waradkar@justdial.com>
2020-05-26 13:53:53 +0530

    integrated css module

--- src/common/containers/App.js
+++ src/common/containers/App.js
@@ -3,15 +3,21 @@ import { Route, Switch } from "react-router-dom";
 
 import loadable from "@loadable/component";
 import "./App.css";
+import styles from "./app.module.css";
 
 const Counter = loadable(() => import("../components/Counter"));
 const About = loadable(() => import("../components/About"));
 
-const App = () => (
-  <Switch>
-    <Route exact path="/" component={Counter} />
-    <Route exact path="/about" component={About} />
-  </Switch>
-);
+const App = () => {
+  return (
+    <>
+      <div className={styles.hello}>Welcome to Razzle.</div>
+      <Switch>
+        <Route exact path="/" component={Counter} />
+        <Route exact path="/about" component={About} />
+      </Switch>
+    </>
+  );
+};
 
 export default App;
