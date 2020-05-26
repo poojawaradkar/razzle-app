80bfe1789cf59addb974ce454c0ea68f0dc74c93
poojawaradkar <pooja.waradkar@justdial.com>
2020-05-26 20:05:00 +0530

    checking lint

--- src/common/containers/App.js
+++ src/common/containers/App.js
@@ -1,26 +1,22 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import loadable from "@loadable/component";
-import "./App.css";
-import styles from "./app.module.css";
+import loadable from '@loadable/component';
+import './App.css';
+import styles from './app.module.css';
 
-const Counter = loadable(() => import("../components/Counter"));
-const About = loadable(() => import("../components/About"));
+const Counter = loadable(() => import('../components/Counter'));
+const About = loadable(() => import('../components/About'));
 
-const App = () => {
-  const a = 'afrgvb';
-    const a = '5';
-  return (
-    <>
-      <div className={styles.hello}>Welcome to Razzle.</div>
-          <p>jdfhskjd</p>
-      <Switch>
-        <Route exact path="/" component={Counter} />
-        <Route exact path="/about" component={About} />
-      </Switch>
-    </>
-  );
-};
+const App = () => (
+  <>
+    <div className={styles.hello}>Welcome to Razzle.</div>
+    <p>jdfhskjd</p>
+    <Switch>
+      <Route exact path="/" component={Counter} />
+      <Route exact path="/about" component={About} />
+    </Switch>
+  </>
+);
 
 export default App;
